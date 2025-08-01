0e60cb90bb01a77f7872bbef6bab35daee5180d9
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-08-01 21:53:56 +0530

    implemented login component.

---
+++ screen-bite/src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.credentials).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('access_token', res.token);
+        // alert('Login successful!');
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        alert('Login failed');
+        console.error(err);
+      }
+    });
+  }
+
+}
